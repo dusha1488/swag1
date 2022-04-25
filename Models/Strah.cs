1e2cdf2b0019a3199e99d39c2caa2926df795ed7
dusha1488 <76405586+dusha1488@users.noreply.github.com>
2022-04-25 12:51:24 +0800

    Add files via upload

---
+++ Models/Strah.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCore.Models
+{
+    public class Strah
+    {
+        
+        public int id_Клиенты { get; set; }
+        public string Имя { get; set; }
+        public string Фамилия { get; set; }
+        public int Возраст { get; set; }
+        public DateTime Дата_рождения  { get; set; }
+        public string Пол { get; set; }
+        public string Адрес { get; set; }
+        public int Серия_номер_паспорта { get; set; }
+        public string Полная_или_неполная_семья { get; set; }
+
+    }
+}
---
+++ Models/personal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCore.Models
+{
+    public class personal
+    {
+        public int id_Персонал { get; set; }
+        public string Имя { get; set; }
+        public string Фамилия { get; set; }
+        public int Возраст { get; set; }
+        public DateTime Дата_рождения { get; set; }
+        public string Пол { get; set; }
+        public string Адрес { get; set; }
+        public int Серия_номер_паспорта { get; set; }
+        public int Зарплата { get; set; }
+    }
+}
